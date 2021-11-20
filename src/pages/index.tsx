ccc7758eeecabfeca92637f94ca429d7f7a3ca82
pajecawav <pajecawav@gmail.com>
2021-11-20 23:07:22 +0300

    use css to conditionally show results popover

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { Input } from "@/components/Input";
 import { useDebouncedState } from "@/hooks/useDebouncedState";
 import { ReposSearchResponse } from "@/types";
-import { Transition } from "@headlessui/react";
 import { SearchIcon } from "@heroicons/react/outline";
 import axios, { AxiosError } from "axios";
 import classNames from "classnames";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Fragment, useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useQuery } from "react-query";
 
 export const HomePage = () => {
@@ -19,7 +18,6 @@ export const HomePage = () => {
 		debounced: debouncedQuery,
 		setState: setQuery,
 	} = useDebouncedState(queryParam, 750);
-	const [showResults, setShowResults] = useState(false);
 
 	const { data: results } = useQuery<ReposSearchResponse, AxiosError>(
 		["search", debouncedQuery],
@@ -46,21 +44,18 @@ export const HomePage = () => {
 
 	return (
 		<div className="w-screen h-screen">
-			<div className="max-w-xl w-full h-full mx-auto flex flex-col gap-4 p-4 justify-center">
+			<div className="max-w-xl w-full h-full mx-auto flex flex-col gap-4 p-4 justify-center group">
 				<div className="flex-grow max-h-[20rem]" />
 
-				<div className="relative flex-shrink-0 group">
+				<div className="relative flex-shrink-0">
 					<Input
-						className="w-full !px-12 !py-3 text-2xl text-center rounded-lg shadow-sm border border-gray-300/80 font-light selection:bg-blue-100"
+						className="w-full !px-12 !py-3 text-2xl text-center rounded-lg shadow-sm border border-gray-300/80 font-light selection:bg-blue-100 group-focus-withing:border-black"
 						value={query}
 						placeholder="Find repo"
 						autoFocus
 						onChange={event => {
 							setQuery(event.target.value);
-							setShowResults(true);
 						}}
-						onFocus={() => debouncedQuery && setShowResults(true)}
-						onBlur={() => setShowResults(false)}
 					/>
 					<div className="absolute top-0 bottom-0 right-2 m-auto w-8 h-8 text-gray-400 transition-colors duration-100 group-focus-within:text-gray-600">
 						<SearchIcon />
@@ -68,45 +63,40 @@ export const HomePage = () => {
 				</div>
 
 				<div className="flex-grow max-h-[20rem]">
-					{/* TODO: use CSS to render results */}
-					<Transition
-						show={showResults && !!results}
-						enter="transition duration-100 ease-out"
-						enterFrom="transform scale-95 opacity-0"
-						leave="transition duration-75 ease-out"
-						leaveTo="transform scale-95 opacity-0"
-						as={Fragment}
+					<div
+						className={classNames(
+							"h-max max-h-full overflow-y-auto border border-gray-200 shadow-sm rounded-lg divide-y",
+							"transition duration-100 ease-out scale-95 opacity-0",
+							results &&
+								"group-focus-within:duration-75 group-focus-within:opacity-100 group-focus-within:scale-100"
+						)}
+						tabIndex={-1}
 					>
-						<div
-							className="h-max max-h-full overflow-y-auto border border-gray-200 shadow-sm rounded-lg divide-y"
-							tabIndex={-1}
-						>
-							{results?.items.map(result => (
-								<Link
-									href={{
-										pathname: `/${result.full_name}`,
-										query: {
-											branch: result.default_branch,
-										},
-									}}
-									key={result.id}
+						{results?.items.map(result => (
+							<Link
+								href={{
+									pathname: `/${result.full_name}`,
+									query: {
+										branch: result.default_branch,
+									},
+								}}
+								key={result.id}
+							>
+								<a
+									className={classNames(
+										"block px-6 py-3 select-none relative cursor-pointer outline-none hover:bg-blue-50 focus:bg-blue-50"
+									)}
 								>
-									<a
-										className={classNames(
-											"block px-6 py-3 select-none relative cursor-pointer hover:bg-blue-50"
-										)}
-									>
-										<div>{result.full_name}</div>
-										{result.description && (
-											<div className="text-sm text-gray-500">
-												{result.description}
-											</div>
-										)}
-									</a>
-								</Link>
-							))}
-						</div>
-					</Transition>
+									<div>{result.full_name}</div>
+									{result.description && (
+										<div className="text-sm text-gray-500">
+											{result.description}
+										</div>
+									)}
+								</a>
+							</Link>
+						))}
+					</div>
 				</div>
 			</div>
 		</div>
