04384bd012b15d61e4a40442463b3753a3d7cab4
pajecawav <pajecawav@gmail.com>
2023-05-07 11:10:18 +0300

    log og-image fetch error

--- src/pages/api/[owner]/[repo]/og-image.tsx
+++ src/pages/api/[owner]/[repo]/og-image.tsx
@@ -35,7 +35,8 @@ export default async function handler(req: NextRequest) {
 	let locs: Locs;
 	try {
 		locs = await getLocs({ owner, repo, branch });
-	} catch {
+	} catch (e) {
+		console.error(e);
 		return new Response("Internal Server Error", {
 			status: 500,
 		});
